302781e6d169e59ccb285b140544533301788d28
Florianschmidtwelzow <florian.schmidt.welzow@t-online.de>
2014-08-29 21:22:29 +0200

    Initial commit
    Change-Id: I206dbc74c17ce13fe1b3394782aa39221bdbf384

---
+++ errorhandler.php
@@ -0,0 +1,74 @@
+<?php
+	// log errors into file
+	ini_set( "log_errors", E_ALL );
+	ini_set( "display_errors", "Off" );
+	ini_set( "error_log", "/var/www/web20/html/specialsources/log/php.errors.log" );
+
+	// set our handlers
+	set_error_handler( 'handleError' );
+	register_shutdown_function( 'shutdownHandler' );
+	/**
+	 * This function handles fatal errors for DroidWiki
+	 */
+	function handleError( $errtype, $errtext, $errfile, $errline ) {
+		$suppress = array( 8, 2048, 8192, 16384 );
+		$excluded_script = array( '/profileinfo.php' );
+		if (
+			!in_array( $errtype, $suppress ) &&
+			!in_array( $_SERVER['SCRIPT_NAME'], $excluded_script )
+		) {
+			// erase last output
+			ob_clean();
+			// show our error page
+			include 'php-fatal-error.template';
+			die();
+		}
+	}
+
+	function FriendlyErrorType( $type ) {
+		switch( $type ) {
+			case E_ERROR: // 1 //
+				return 'E_ERROR';
+			case E_WARNING: // 2 //
+				return 'E_WARNING';
+			case E_PARSE: // 4 //
+				return 'E_PARSE';
+			case E_NOTICE: // 8 //
+				return 'E_NOTICE';
+			case E_CORE_ERROR: // 16 //
+				return 'E_CORE_ERROR';
+			case E_CORE_WARNING: // 32 //
+				return 'E_CORE_WARNING';
+			case E_CORE_ERROR: // 64 //
+				return 'E_COMPILE_ERROR';
+			case E_CORE_WARNING: // 128 //
+				return 'E_COMPILE_WARNING';
+			case E_USER_ERROR: // 256 //
+				return 'E_USER_ERROR';
+			case E_USER_WARNING: // 512 //
+				return 'E_USER_WARNING';
+			case E_USER_NOTICE: // 1024 //
+				return 'E_USER_NOTICE';
+			case E_STRICT: // 2048 //
+				return 'E_STRICT';
+			case E_RECOVERABLE_ERROR: // 4096 //
+				return 'E_RECOVERABLE_ERROR';
+			case E_DEPRECATED: // 8192 //
+				return 'E_DEPRECATED';
+			case E_USER_DEPRECATED: // 16384 //
+				return 'E_USER_DEPRECATED';
+		}
+		return "fatal";
+	}
+
+	function shutdownHandler() {
+		$last_error = error_get_last();
+		if ( !empty( $last_error ) ) {
+			handleError(
+				$last_error['type'],
+				$last_error['message'],
+				$last_error['file'],
+				$last_error['line']
+			);
+		}
+	}
